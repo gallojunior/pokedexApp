a29569ca4b08e21741de3fc6346434dc131960d9
GalloJunior <gallojunior@gmail.com>
2024-03-27 22:31:46 -0300

    Testes

--- pokedex/src/app/index.js
+++ pokedex/src/app/index.js
@@ -1,10 +1,12 @@
+import { Link } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Home() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Home!</Text>
+      <Link href="details">Details</Link>
       <StatusBar style="auto" />
     </View>
   );
